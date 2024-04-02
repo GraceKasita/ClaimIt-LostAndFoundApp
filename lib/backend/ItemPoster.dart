b9deb9023de04dec4fc28ffcfd3941d04db780c7
Theint Nandar Su <63658719+tnandarsu@users.noreply.github.com>
2024-04-02 23:06:22 +0700

    Update ItemPoster.dart

--- lib/backend/ItemPoster.dart
+++ lib/backend/ItemPoster.dart
@@ -3,7 +3,7 @@ import '../backend/Item.dart';
 abstract class ItemPoster {
   Future<void> post(Item newItem);
   Future<void> sendMatchingItemEmail(String recipientEmail, Item lostItem,
-      Item foundItem, double similarity, String foundItemImagePath);
+      Item foundItem, String fsimilarity, String foundItemImagePath);
 
   Future<void> matchAndNotify(Item newItem);
 }
